17906f52f063abecfe81d86e5ca770469cc874a4
Richard Beattie <richard.beattie@aol.co.uk>
2019-03-17 21:35:13 +0000

    Heterodyne
    A simple script which uses sox to mix an audio in the aim of causing a heterodyne effect

---
+++ heterodyne.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# hetrodyne - A script to mix an audio file with a higher frequency in order to produce the hetrodyne effect
+
+usage()
+{
+  echo "usage: hetrodyne -i file -n file | [-h]"
+}
+
+#Command vars
+input=
+noise=
+output="output.wav"
+
+#MAIN
+
+while [ "$1" != "" ]; do
+  case $1 in
+      -i | --input )      shift
+                          input=$1
+                          ;;
+      -n | --noise )      shift
+                          noise=$1
+                          ;;
+      -o | --output )     shift
+                          output=$1
+                          ;;
+      -h | --help )       usage
+                          exit
+                          ;;
+      * )                 usage
+                          exit 1
+    esac
+    shift
+done
+
+if [$input == ""];
+then
+  echo "No input file specified"
+  usage
+  exit 1
+fi
+
+if [$noise == ""];
+then
+  echo "No noise profile specified"
+  usage
+  exit 1
+fi
+
+sox $input -t wav - gain -n -0.9 | sox -t wav - -t wav - noisered $noise 0.15 | sox -M -t wav - 32k.wav $output
